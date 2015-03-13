d09c5b5c3cfe534bd6daf6b26c87dbeb0490045b
Aaron Parecki <aaron@parecki.com>
2015-03-13 12:29:37 -0700

    add lib folder

---
+++ lib/config.template.php
@@ -0,0 +1,13 @@
+<?php
+class Config {
+  public static $hostname = 'reader.dev';
+  public static $ssl = false;
+
+  public static $dbHost = '127.0.0.1';
+  public static $dbName = 'reader';
+  public static $dbUsername = 'reader';
+  public static $dbPassword = '';
+
+  public static $defaultAuthorizationEndpoint = 'https://indieauth.com/auth';
+}
+
---
+++ lib/helpers.php
@@ -0,0 +1,138 @@
+<?php
+ORM::configure('mysql:host=' . Config::$dbHost . ';dbname=' . Config::$dbName);
+ORM::configure('username', Config::$dbUsername);
+ORM::configure('password', Config::$dbPassword);
+
+function friendly_url($url) {
+  return preg_replace(['/https?:\/\//','/\/$/'],'',$url);
+}
+
+// Input: Any URL or string like "aaronparecki.com"
+// Output: Normlized URL (default to http if no scheme, force "/" path)
+//         or return false if not a valid URL
+function normalize_url($url) {
+  $me = parse_url($url);
+
+  if(array_key_exists('path', $me) && $me['path'] == '')
+    return false;
+
+  // parse_url returns just "path" for naked domains
+  if(count($me) == 1 && array_key_exists('path', $me)) {
+    $me['host'] = $me['path'];
+    unset($me['path']);
+  }
+
+  if(!array_key_exists('scheme', $me))
+    $me['scheme'] = 'http';
+
+  if(!array_key_exists('path', $me))
+    $me['path'] = '/';
+
+  // Invalid scheme
+  if(!in_array($me['scheme'], array('http','https')))
+    return false;
+
+  return build_url($me);
+}
+
+function render($page, $data) {
+  global $app;
+  return $app->render('layout.php', array_merge($data, array('page' => $page)));
+};
+
+function partial($template, $data=array(), $debug=false) {
+  global $app;
+
+  if($debug) {
+    $tpl = new Savant3(\Slim\Extras\Views\Savant::$savantOptions);
+    echo '<pre>' . $tpl->fetch($template . '.php') . '</pre>';
+    return '';
+  }
+
+  ob_start();
+  $tpl = new Savant3(\Slim\Extras\Views\Savant::$savantOptions);
+  foreach($data as $k=>$v) {
+    $tpl->{$k} = $v;
+  }
+  $tpl->display($template . '.php');
+  return ob_get_clean();
+}
+
+function json_response(&$app, $response) {
+  $app->response()['Content-Type'] = 'application/json';
+  $app->response()->body(json_encode($response));
+}
+
+function session($key) {
+  if(array_key_exists($key, $_SESSION))
+    return $_SESSION[$key];
+  else
+    return null;
+}
+
+function k($a, $k, $default=null) {
+  if(is_array($k)) {
+    $result = true;
+    foreach($k as $key) {
+      $result = $result && array_key_exists($key, $a);
+    }
+    return $result;
+  } else {
+    if(is_array($a) && array_key_exists($k, $a) && $a[$k])
+      return $a[$k];
+    elseif(is_object($a) && property_exists($a, $k) && $a->$k)
+      return $a->$k;
+    else
+      return $default;
+  }
+}
+
+function get_timezone($lat, $lng) {
+  try {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://timezone-api.geoloqi.com/timezone/'.$lat.'/'.$lng);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    $tz = @json_decode($response);
+    if($tz)
+      return new DateTimeZone($tz->timezone);
+  } catch(Exception $e) {
+    return null;
+  }
+  return null;
+}
+
+function micropub_post($endpoint, $params, $access_token, $h='entry') {
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $endpoint);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Authorization: Bearer ' . $access_token
+  ));
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array_merge(array(
+    'h' => $h
+  ), $params)));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, true);
+  $response = curl_exec($ch);
+  $error = curl_error($ch);
+  return array(
+    'response' => $response,
+    'error' => $error,
+    'curlinfo' => curl_getinfo($ch)
+  );
+}
+
+function relative_time($date) {
+  static $rel;
+  if(!isset($rel)) {
+    $config = array(
+      'language' => '\RelativeTime\Languages\English',
+      'separator' => ', ',
+      'suffix' => true,
+      'truncate' => 1,
+    );
+    $rel = new \RelativeTime\RelativeTime($config);
+  }
+  return $rel->timeAgo($date);
+}
