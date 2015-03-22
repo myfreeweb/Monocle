fe621b1bbcc23489c57604223ab2388144b11a23
Aaron Parecki <aaron@parecki.com>
2015-03-22 19:13:27 +0000

    fix home page icon

--- views/index-public.php
+++ views/index-public.php
@@ -1,7 +1,7 @@
 <div class="narrow">
 
   <div class="jumbotron h-x-app">
-    <h1><img src="/images/icons/apple-icon-144x144.png" height="72" style="margin-bottom: 13px;" class="u-logo p-name" alt="Monocle"> Monocle</h1>
+    <h1><img src="/images/icons/monocle-icon-84.png" height="84" style="margin-bottom: 13px;" class="u-logo p-name" alt="Monocle"> Monocle</h1>
 
     <p class="tagline p-summary">Monocle is an IndieWeb-enabled reader.</p>
 
