01fd974838a801c43a6c43204dec0ac27aacab92
Aaron Parecki <aaron@parecki.com>
2015-04-22 11:00:09 -0700

    clarify error when no auth_endpoint or micropub_endpoint is specified

--- views/auth_start.php
+++ views/auth_start.php
@@ -2,11 +2,12 @@
   <?= partial('partials/header') ?>
 
   <?php if($this->authorizationEndpoint): ?>
-    <div class="bs-callout bs-callout-warning">Monocle found your authorization endpoint, but you didn't specify a micropub endpoint.</div>
+    <div class="bs-callout bs-callout-warning">Monocle found your authorization endpoint, but you didn't specify a Micropub endpoint.</div>
     <p>You'll still be able to read with Monocle, but won't be able to use it to reply or to "like" things from your site.</p>
   <?php else: ?>
-    <div class="bs-callout bs-callout-warning">It looks like your site isn't pointing to an authorization server.</div>
+    <div class="bs-callout bs-callout-warning">It looks like your site isn't pointing to a Micropub endpoint.</div>
     <p>You'll still be able to read with Monocle, but won't be able to use it to reply or to "like" things from your site.</p>
+    <p><a href="https://quill.p3k.io/docs">Read more about how to create a Micropub endpoint</a>.</p>
   <?php endif; ?>
 
   <p><a href="<?= $this->authorizationURL ?>" class="btn btn-primary">Sign In</a></p>
